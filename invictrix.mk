e9bdb0ed1e90115a25ef0897c851aecfd1d39614
me2151 <dillonbrosche@gmail.com>
2018-08-05 16:36:31 -0400

    Invictify

--- invictrix.mk
+++ invictrix.mk
@@ -1,19 +1,19 @@
 $(call inherit-product, device/lge/h930/full_h930.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Invictrix stuff.
+$(call inherit-product, vendor/invictrix/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h930
+PRODUCT_NAME := invictrix_h930
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
     PRODUCT_DEVICE="joan" \
     PRODUCT_NAME="joan_global_com" \
-    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 1819221128e3d release-keys"
+    PRIVATE_BUILD_DESC="joan_global_com-user 8.1.0 OPM6.171019.030.E1 1819221128e3d release-keys"
 
-BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/1819221128e3d:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_global_com/joan:8.1.0/OPM6.171019.030.E1/1819221128e3d:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model
